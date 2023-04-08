22320e061169417631f1cce02695753e7aefe3eb
ayseekinerdur <ekinerdurr@gmail.com>
2023-04-08 13:58:44 +0300

    week 3

---
+++ Week3RemainderOperator/Program.cs
@@ -0,0 +1,35 @@
+﻿//Odd or even
+
+using System.Timers;
+
+int a = 17;
+
+int result = a % 2;
+
+Console.WriteLine(result);
+
+//if(result == 0)
+//{
+
+//    Console.WriteLine($"{a} is an even number");
+ 
+//} else
+//{
+
+//    Console.WriteLine($"{a} is an odd number");
+//}
+
+switch(result)
+{
+
+    case 0:
+        Console.WriteLine($"{a} is an even number");
+        break;
+    case 1:
+        Console.WriteLine($"{a} is an odd number");
+        break;
+    default:
+        Console.WriteLine("Invalid Value");
+        break;
+
+}
